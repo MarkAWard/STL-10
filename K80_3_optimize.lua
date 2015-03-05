f338fa6485e786e85e3140b5009a97951f2b016d
Lefteris Soulas <elnio@Lefteriss-MacBook-Air.local>
2015-03-05 14:03:00 -0500

    changed variable i to epoch

--- K80_3_optimize.lua
+++ K80_3_optimize.lua
@@ -3,16 +3,17 @@ logger = optim.Logger(paths.concat('results', 'accuracyResults.log'))
 logger:add{"EPOCH    TRAIN ERROR    VAL ERROR"}
 
 valErrorEpochPair = {1.1,-1}
-for i =1, opt.epochs do
-	print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> EPOCH " .. i .. " <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<") 
-	trainErr = train(i)
-	valErr   = evaluate( paths.concat('results','model_'..epoch..'.net'), valData, false)
+for epoch =1, opt.epochs do
+	print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> EPOCH " .. epoch .. " <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<") 
+	trainErr = train( epoch )
+	valErr   = evaluate( paths.concat('results','model_'.. epoch ..'.net'), valData, false)
 	if valErr < valErrorEpochPair[1] then
 		valErrorEpochPair[1] = valErr
-		valErrorEpochPair[2] = i
+		valErrorEpochPair[2] = epoch
 	end
-	logger:add{i .. "    " .. trainErr .. "    " ..  valErr}
+	logger:add{epoch .. "    " .. trainErr .. "    " ..  valErr}
 end
 
+print("Now testing on model no. " .. valErrorEpochPair[2] .. " with validation error= " .. valErrorEpochPair[1])
 bestModelPath = paths.concat('results','model_'.. valErrorEpochPair[2] ..'.net')
 evaluate( bestModelPath, testData, true)
