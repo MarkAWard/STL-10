878237696aa7a18b1abbebf2c0c36ce2cba7c770
Mark Ward <markmaw@gmail.com>
2015-03-07 20:14:34 -0500

    now i added the training procedure

--- generic_pipeline.lua
+++ generic_pipeline.lua
@@ -197,8 +197,120 @@ print('==> setting model and criterion')
 model = mod.select_model(opt)
 criterion = crit.select_criterion(opt)
 
-print(trainData:size())
+print('Size of training data: ' .. trainData:size())
 print(model)
 print(criterion)
 
 
+
+----------------------------------- TRAIN FUNCTION --------------------------------------
+
+function train( epoch )
+	classes = {'1','2','3','4','5','6','7','8','9','0'}
+	local confusion = optim.ConfusionMatrix(classes)
+	model:training() -- set model to training mode (for modules that differ in training and testing, like Dropout)
+	-- Shuffling the training data   
+	shuffle = torch.randperm(trainData:size())
+	shuffed_tr_data=torch.zeros(trainData:size(),channels,imageHeight,imageWidth)
+	shuffed_tr_targets=torch.zeros(trainData:size())	
+	for t = 1, trainData:size() do
+		shuffed_tr_data[t]=trainData.data[shuffle[t]]
+		shuffed_tr_targets[t]=trainData.labels[shuffle[t]]
+	end
+	
+	-- batch training to exploit CUDA optimizations
+	parameters,gradParameters = model:getParameters()
+	local clr = 0.1
+	local no_wrong=0
+	for t = 1,trainData:size(), opt.batchSize do
+		local inputs  = shuffed_tr_data[{{t, math.min(t+opt.batchSize-1, trainData:size())}}]
+		local targets = shuffed_tr_targets[{{t, math.min(t+opt.batchSize-1, trainData:size())}}]
+		if opt.type=='cuda' then 
+			inputs=inputs:cuda()
+			targets=targets:cuda()
+		end
+		gradParameters:zero()
+		
+		local output = model:forward(inputs)
+		local f = criterion:forward(output, targets)
+		local trash, argmax = output:max(2)
+	  	if opt.type=='cuda' then  argmax=argmax:cuda() else argmax=argmax:float() end
+	  	
+	  	no_wrong = no_wrong + torch.ne(argmax, targets):sum()
+	  	model:backward(inputs, criterion:backward(output, targets))
+
+		--clr = opt.learningRate * (0.5 ^ math.floor(epoch / opt.lrDecay))
+
+		clr = opt.learningRate
+		
+		
+		parameters:add(-clr, gradParameters)
+		
+		argmax=argmax:reshape((#inputs)[1])
+		confusion:batchAdd(argmax, targets)
+   end
+
+   local filename = paths.concat('results', 'model_' .. epoch .. '.net')
+   os.execute('mkdir -p ' .. sys.dirname(filename))
+   torch.save(filename, model)
+   --print(confusion)
+   return no_wrong/(trainData:size())   
+end
+--------------------------------- END TRAIN FUNCTION --------------------------------
+
+-------------------------------- EVALUATE FUNCTION --------------------------------------
+function evaluate( modelPath, dataset, writeToFile)
+	modelToEval = torch.load(modelPath)
+	local f
+	if writeToFile then
+	   local outputFile = paths.concat('results', 'output.csv')
+	   f = io.open(outputFile, "w")
+	   f:write("Id,Category\n")
+	end
+	
+	modelToEval:evaluate()
+	local no_wrong = 0
+	for t = 1,dataset:size(), opt.batchSize do
+		local inputs  = dataset.data[{{t, math.min(t+opt.batchSize-1, dataset:size())}}]
+		local targets = dataset.labels[{{t, math.min(t+opt.batchSize-1, dataset:size())}}]
+		if opt.type == 'cuda' then 
+			inputs  = inputs:cuda() 
+			targets = targets:cuda()
+    	end
+    	local output = modelToEval:forward(inputs)
+    	local trash, argmax = output:max(2)
+    	no_wrong = no_wrong + torch.ne(argmax, targets):sum()
+    	
+    	if writeToFile then
+    		for idx = 1, opt.batchSize do
+    			f:write( t+idx-1 .. " , " .. argmax[idx][1] .. "\n") 
+    		end
+    	end 
+
+    end
+	if writeToFile then f:close() end
+    return no_wrong/(dataset:size())
+end
+
+
+
+
+logger = optim.Logger(paths.concat('results', 'errorResults.log'))
+logger:add{"EPOCH    TRAIN ERROR    VAL ERROR"}
+
+valErrorEpochPair = {1.1,-1}
+for epoch =1, opt.epochs do
+	print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> EPOCH " .. epoch .. " <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<") 
+	trainErr = train( epoch )
+	print(trainErr)
+	valErr   = evaluate( paths.concat('results','model_'.. epoch ..'.net'), valData, false)
+	if valErr < valErrorEpochPair[1] then
+		valErrorEpochPair[1] = valErr
+		valErrorEpochPair[2] = epoch
+	end
+	logger:add{epoch .. "    " .. trainErr .. "    " ..  valErr}
+end
+
+print("Now testing on model no. " .. valErrorEpochPair[2] .. " with validation error= " .. valErrorEpochPair[1])
+bestModelPath = paths.concat('results','model_'.. valErrorEpochPair[2] ..'.net')
+evaluate( bestModelPath, testData, true)
